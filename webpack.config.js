035ccfd7b00728792f67285ecd0dced37aa7925c
Sher Minn Chong <sherminn.chong@gmail.com>
2015-11-03 17:52:45 -0500

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'public'),
-        publicPath: '/',
+        publicPath: '',
         filename: 'bundle.js'
     },
     module: {
@@ -27,6 +27,30 @@ module.exports = {
             {
                 test: /\.(sass|scss)$/,
                 loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+            },
+            // LESS
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+            },
+            // Json
+            {
+                test: /\.json$/,
+                loader: 'json'
+            },
+            // Images
+            {
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$/i, 
+                loader: 'file'
+            },
+            // Fonts
+            { 
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: "url-loader?limit=10000&minetype=application/font-woff" 
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: "file-loader" 
             }
         ]
     },
