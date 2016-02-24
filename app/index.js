70c722c3f569899d5cb8401679e22580810cd10e
Sher Minn Chong <sherminn.chong@gmail.com>
2016-02-24 16:10:32 +0800

    Draw overlay on different canvas instead

--- app/index.js
+++ app/index.js
@@ -3,19 +3,29 @@ import 'styles/style.scss'
 
 const options = {
     gridSize: {
-        x: 4*4,
-        y: 3*4,
+        x: 4*6,
+        y: 3*6,
     },
     debug: true,
     pixelDiffThreshold: 0.4,
-    movementThreshold: 0.01,
-    fps: 30
+    movementThreshold: 0.0012,
+    fps: 20
 }
+
+var overlay = document.getElementById('overlay');
+const ctx = overlay.getContext('2d');
+let timeoutClear;
 
 const md = new MotionDetect('src', 'dest', options);
 
 // on motion detected, draw grid
-md.onDetect((ctx, data) => {
+md.onDetect((other, data) => {
+    clearTimeout(timeoutClear);
+
+    const canvas = ctx.canvas;
+    canvas.width = other.canvas.width;
+    canvas.height = other.canvas.height;
+
     ctx.save();
     const grid = data.motions;
     const gs = data.gd.size;
@@ -41,5 +51,9 @@ md.onDetect((ctx, data) => {
     });
 
     ctx.restore();
+
+    timeoutClear = setTimeout(()=>{
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }, 100);
     
 })
